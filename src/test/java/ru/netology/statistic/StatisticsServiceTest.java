6a55fe9120bcb99667c61db250a110f08fa74977
MECCNR <e.migalckin@yandex.ru>
2022-08-01 13:41:05 +0900

    super

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -2,7 +2,7 @@ package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StatisticsServiceTest {
 
@@ -12,6 +12,17 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxAfter() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 2};
+        long expected = 2;
 
         long actual = service.findMax(incomesInBillions);
 

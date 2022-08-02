8dac8fb87ecf9358cda43cb74daf0062cadc69df
MECCNR <e.migalckin@yandex.ru>
2022-08-02 11:41:51 +0900

    super

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,6 +17,7 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
     @Test
     void findMaxAfter() {
         StatisticsService service = new StatisticsService();
